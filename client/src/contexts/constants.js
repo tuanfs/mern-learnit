208428dcbda4f91b7c4ebf596ec81ad29787e8e2
Lê Văn Tuấn <tuan73954@st.vimaru.edu.vn>
2021-12-05 12:46:59 +0700

    make it betterD

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
-        : 'deployURL';
+        : 'https://radiant-ocean-31499.herokuapp.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
 
